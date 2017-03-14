0a7b2bcda430d1512ee2a2d8332e856d184d3ac0
Cs8003 <1chinmay.singh@gmail.com>
2017-03-14 18:58:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 var button = document.getElementById('counter');
 
-
+var counter = 0;
 button.onclick = function() {
   
   var request= new XMLHttpRequest();
@@ -12,6 +12,7 @@ button.onclick = function() {
             var counter = request.responseText;
     var span = document.getElementById('count');
   span.innerHTML = counter.toString();
+  counter = counter + 1;
         }
     }  
   };
