aa37917fb8a144b0e2d68b2febe0b40075aab05d
Cs8003 <1chinmay.singh@gmail.com>
2017-03-14 16:34:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ var element = document.getElementById('main-text');
  element.innerHTML = 'New value';
 
 
-
+//
 var img = document.getElementById('madi');
-var marginLeft = 0;
+marginLeft=0;
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
