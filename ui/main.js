bc0315cca32643aa1d7f62fa151e81a2cdad3205
Cs8003 <1chinmay.singh@gmail.com>
2017-03-14 16:52:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,2 @@
 console.log('Loaded!');
 
-//                  
-
-
-var element = document.getElementById('main-text');
- element.innerHTML = 'New value';
-
-
-//
-var img = document.getElementById('madi');
-marginLeft=0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-    }
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-    
-};
